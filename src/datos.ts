fb8d3772a2f70a56f79f170bac308ec0a55152fe
CARMENSOSA19 <104264514+CARMENSOSA19@users.noreply.github.com>
2022-04-28 19:01:00 -0300

    Initial commit

---
+++ src/datos.ts
@@ -0,0 +1,8 @@
+let btnEnv = document.getElementById("btnEnviar");
+let dato = document.getElementById("dato");
+let rotulo = document.getElementById("rotulo");
+btnEnv.addEventListener("click", () => {
+  //tu codigo va aqui
+
+  console.log("el dato ingresado es", dato.value);
+});
